0ed376449c6061d988ddbae308230138ec78ce5c
Mohammad Maroof <mmaroof487@gmail.com>
2024-09-29 23:26:14 +0530

    fix: navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LogIn from "./Login.jsx";
 import Home from "./Home.jsx";
 
 export default function App() {
- const [loggedIn, setLoggedIn] = useState(false);
+ const [loggedIn, setLoggedIn] = useState(true);
 
  return <>{loggedIn ? <Home setLoggedIn={setLoggedIn} /> : <LogIn setLoggedIn={setLoggedIn} />}</>;
 }
